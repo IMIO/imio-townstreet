674c1436907e8cd4a2f75052413332cd8a37d85d
Christophe Boulanger <christophe.boulanger@imio.be>
2020-06-18 16:33:15 +0200

    Some changes about python3 and new version of wcs

--- imio_town_street/install_imio-town-street.sh
+++ imio_town_street/install_imio-town-street.sh
@@ -10,7 +10,7 @@ install_path="/usr/lib/imio_town_street"
 
 # WCS : Get wcs tenant
 # nomcommune-formulaires.nomdomain
-wcs_tenant=$(python $install_path/get-wcs-tenant.py 2>&1)
+wcs_tenant=$(python3 $install_path/get-wcs-tenant.py 2>&1)
 
 commune=$(echo "$wcs_tenant" | sed "s/-formulaires.$domain//")
 
@@ -26,7 +26,7 @@ category_registration_number=$(ls /var/lib/wcs/$wcs_tenant/categories |  sort -n
 sed -i 's/id="0"/id="'$(($category_registration_number + 1))'"/g' $install_path/category/0
 mv $install_path/category/0 $install_path/category/$(($category_registration_number + 1))
 cp $install_path/category/$(($category_registration_number + 1)) /var/lib/wcs/$wcs_tenant/categories/$(($category_registration_number + 1))
-chown -R ${USER}:${USER} /var/lib/wcs/$wcs_tenant/categories/$(($category_registration_number + 1)) 
+chown -R ${USER}:${USER} /var/lib/wcs/$wcs_tenant/categories/$(($category_registration_number + 1))
 
 if ! [ -d /var/lib/wcs/$wcs_tenant/wscalls/ ]
 then
@@ -38,11 +38,11 @@ then
 fi
 
 # WCS : Script to import xml workflow in wcs (Workflows must be create before forms)
-sudo -u  wcs wcsctl -f /etc/wcs/wcs-au-quotidien.cfg runscript --vhost=$wcs_tenant $install_path/import-workflows.py $install_path
+sudo -u wcs wcs-manage runscript --all-tenants $install_path/import-workflows.py $install_path
 
 # WCS : Script to import xml forms in wcs
-sed -i 's/category_id="0"/category_id="'$(($category_registration_number + 1))'"/g' $install_path/forms/form-town-street.wcs
-sudo -u  wcs wcsctl -f /etc/wcs/wcs-au-quotidien.cfg runscript --vhost=$wcs_tenant $install_path/import-forms.py $install_path
+sed -i 's/category_id="0"/category_id="'$(($category_registration_number + 1))'"/g' $install_path/forms/form-imio-town-street.wcs
+sudo -u wcs wcs-manage runscript --all-tenants $install_path/import-forms.py $install_path
 
 # COMBO : import town-street page (settings.json)
 sed -i "s/\[COMMUNE\]/$commune/g" $install_path/combo/combo-town-street.json
