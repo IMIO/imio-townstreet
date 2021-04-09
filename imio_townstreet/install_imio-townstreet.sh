074da51b21e385de479e9ae7d23dfca86d5f8e60
Daniel Muyshond <daniel.muyshond@imio.be>
2021-04-09 16:11:28 +0200

    [TOWS-33] upd bash install and delete old cells

--- imio_townstreet/install_imio-townstreet.sh
+++ imio_townstreet/install_imio-townstreet.sh
@@ -3,6 +3,6 @@
 set -e # Exit immediately if a command exits with a non-zero status.
 
 # installation path
-install_path="/usr/lib/imio_townstreet"
+install_path="/usr/lib/imio_town_street"
 
 sudo -u hobo hobo-manage imio_indus_deploy ---directory $install_path
