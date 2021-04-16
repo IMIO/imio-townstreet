143ff1f87e756f8b884450c9d17548457e0d7ff3
Daniel Muyshond <daniel.muyshond@imio.be>
2021-04-16 16:16:39 +0200

    [TOWS-33] rename folder (2nd)

---
+++ imio_townstreet/import-workflows.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+from wcs.workflows import Workflow
+
+folder_path = '{0}/workflows/'.format(sys.argv[1])
+for fichier in os.listdir(folder_path):
+    if fichier[-4:] == ".wcs":
+        fd = open("{}{}".format(folder_path, fichier))
+        wf = Workflow.import_from_xml(fd)
+        wf.store()
