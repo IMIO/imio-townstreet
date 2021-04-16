2cbd442e183288eb85e1fc8a52994114b601b1d2
Daniel Muyshond <daniel.muyshond@imio.be>
2021-04-16 17:19:47 +0200

    [TOWS-33] delete old files

--- imio_townstreet/install_imio-townstreet.sh
+++ imio_townstreet/install_imio-townstreet.sh
@@ -3,6 +3,6 @@
 set -e # Exit immediately if a command exits with a non-zero status.
 
 # installation path
-install_path="/usr/lib/imio_town_street"
+install_path="/usr/lib/imio_townstreet"
 
 sudo -u hobo hobo-manage imio_indus_deploy ---directory $install_path
