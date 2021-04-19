fff2e66a2d29c63ea13a6139dd545c218d6bb746
Daniel Muyshond <daniel.muyshond@imio.be>
2021-04-19 15:30:17 +0200

    [TOWS-33] remove packages + auto clean code via black

--- setup.py
+++ setup.py
@@ -6,31 +6,32 @@ from setuptools.command.install import install
 from setuptools import setup
 import os
 
+
 class inst(install):
     def run(self):
         install.run(self)
-        path = os.getcwd().replace(" ", r"\ ").replace("(",r"\(").replace(")",r"\)") + "/bin/"
-        os.system("sh "+path+"install_imio-townstreet.sh")
+        path = os.getcwd().replace(" ", r"\ ").replace("(", r"\(").replace(")", r"\)") + "/bin/"
+        os.system("sh " + path + "install_imio-townstreet.sh")
+
 
 setup(
-    name='imio-townstreet',
-    author='Christophe Boulanger',
-    author_email='christophe.boulanger@imio.be',
-    packages=["publik-imio-industrialisation"],
+    name="imio-townstreet",
+    author="Christophe Boulanger",
+    author_email="christophe.boulanger@imio.be",
     include_package_data=True,
-    url='https://github.com/IMIO/imio-townstreet',
+    url="https://github.com/IMIO/imio-townstreet",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
     ],
     zip_safe=False,
     cmdclass={
-        'inst': inst,
-    }
+        "inst": inst,
+    },
 )
