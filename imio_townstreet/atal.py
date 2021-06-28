eb89a0979a462b01918abdbad41a534a24c427f3
Daniel Muyshond <daniel.muyshond@imio.be>
2021-06-28 17:01:53 +0200

    [TOWS-17] add atal connector check to setup

---
+++ imio_townstreet/atal.py
@@ -0,0 +1,3 @@
+if "passerelle_imio_ia_tech" not in INSTALLED_APPS:
+    INSTALLED_APPS += ("passerelle_imio_ia_tech",)
+    TENANT_APPS += ("passerelle_imio_ia_tech",)
--- imio_townstreet/install_imio-townstreet.sh
+++ imio_townstreet/install_imio-townstreet.sh
@@ -2,6 +2,15 @@
 
 set -e # Exit immediately if a command exits with a non-zero status.
 
+# setup passerelle connector
+FILE=/etc/passerelle/settings.d/atal.py
+if test -f "$FILE"; then
+    echo "$FILE already exists. This is what we want so that's fine !"
+else
+    echo "$FILE does not exist yet so I'm gonna create it and reboot passerelle..."
+    cp $(pwd)/atal.py /etc/passerelle/settings.d/ && service passerelle restart
+fi
+
 # installation path
 install_path="/usr/lib/imio_townstreet"
 
